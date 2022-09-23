f258078892133ea1a0907a531d99b872a8f32a05
dk188 <dk188@LAPTOP-UVIC0HB6>
2022-09-23 02:15:11 -0600

    Agregar archivos de proyecto.

---
+++ SUMA_PROPIEDADES/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static SUMA_PROPIEDADES.SUMA;
+
+namespace SUMA_PROPIEDADES
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Instancia de la clase cloperaciones.
+            cloperaciones osuma = new cloperaciones();
+            // Se le asigna a la propiedad N1 el valor dado en la textBox1.
+            osuma.N1 = int.Parse(textBox1.Text);
+            // Se le asigna a la propiedad N2 el valor dado en la textBox2.
+            osuma.N2 = int.Parse(textBox2.Text);
+            // Se le asigna a la textBox3 el resultado que se le aplica al objeto osuma con el metodo Sumar y se pasa a tipo string.
+            textBox3.Text = osuma.Sumar().ToString();
+        }
+    }
+}
---
+++ SUMA_PROPIEDADES/SUMA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUMA_PROPIEDADES
+{
+    internal class SUMA
+    {
+        internal class cloperaciones
+        {
+            // Propiedades, campos, etc.
+            private int n1;
+            private int n2;
+            
+            // Encapsulamiento de las propiedades.
+            public int N1 { get => n1; set => n1 = value; }
+            public int N2 { get => n2; set => n2 = value; }
+
+            // Metodo
+            public int Sumar()
+            {
+                return n1 + n2;
+            }
+        }
+    }
+}
